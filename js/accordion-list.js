bfff1310ca268b6ac303537444ef45fc099e0a34
bmoreto <beatriz_moreto@hotmail.com>
2024-04-02 06:22:02 -0300

    js

---
+++ js/accordion-list.js
@@ -0,0 +1,17 @@
+function initAccordion() {
+  const accordionList = document.querySelectorAll(".js-accordion dt");
+  const activeClass = "ativo";
+
+  if (accordionList.length) {
+    function activeAccordion() {
+      this.classList.toggle(activeClass);
+      this.nextElementSibling.classList.toggle(activeClass);
+    }
+
+    accordionList.forEach((item) => {
+      item.addEventListener("click", activeAccordion);
+    });
+  }
+}
+
+initAccordion();
---
+++ js/scroll-suave-link-interno.js
@@ -0,0 +1,20 @@
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('.menu-lista a[href^="#"]');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
+}
+
+initScrollSuave();
